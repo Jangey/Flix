e5dabaca77361008f98ef231386de9b7ca80566f
Jangey <jangey.lu@gmail.com>
2018-09-10 20:29:35 -0700

    week 2 NO.2 Collection View Setup

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Jangey Lu on 9/10/18.
+//  Copyright © 2018 Jangey Lu. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
